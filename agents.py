0bae1856754a052ea7c3c10111bb546176188ef2
Kyntaz <ghost.615b@gmail.com>
2019-03-22 19:44:01 +0000

    add functions to calculate the pondered value of tasks and flesh things out

--- agents.py
+++ agents.py
@@ -1,6 +1,8 @@
 import re
 
+# ===
 # Internal Lottery Implementation:
+# ===
 
 class Outcome:
     """
@@ -24,6 +26,31 @@ class Outcome:
                 self.occurrences = info[0]
                 self.value = info[1]
                 
+    def is_composite(self):
+        return len(self.children) > 0
+    
+    def is_belief(self):
+        return self.occurrences == 0
+    
+    def calculate_pondered_value(self):
+        if not self.is_composite():
+            if self.is_belief(): return self.value * self.probability
+            else: return self.value * self.occurrences
+        else:
+            has_hard_evidence = False
+            comp_value = 0
+            for child in self.children.values():
+                if child.is_belief():
+                    if not has_hard_evidence: comp_value += child.calculate_pondered_value()
+                else:
+                    if not has_hard_evidence:
+                        has_hard_evidence = True
+                        comp_value = child.calculate_pondered_value()
+                    else:
+                        comp_value += child.calculate_pondered_value()
+            return comp_value
+            
+                
 class Task:
     """
     This represents a task that an agent can execute.
@@ -34,6 +61,20 @@ class Task:
         self.outcomes = {}
         for k in info:
             self.outcomes[k] = Outcome(k, info[k])
+        
+    def calculate_pondered_value(self):
+        has_hard_evidence = False
+        pond_value = 0
+        for outcome in self.outcomes.values():
+            if outcome.is_belief():
+                if not has_hard_evidence: pond_value += outcome.calculate_pondered_value()
+            else:
+                if not has_hard_evidence:
+                    has_hard_evidence = True
+                    pond_value = outcome.calculate_pondered_value()
+                else:
+                    pond_value += outcome.calculate_pondered_value()
+        return pond_value        
             
 class Lottery:
     """
@@ -44,6 +85,12 @@ class Lottery:
         self.tasks = {}
         for k in info:
             self.tasks[k] = Task(k, info[k])
+            
+    def calculate_pondered_values(self):
+        vals = {}
+        for k in self.tasks:
+            vals[k] = self.tasks[k].calculate_pondered_value()
+        return vals
 
 def parse_lottery(s):
     # This is just a hack so that I don't have to waste time writting a parser for the lottery
@@ -58,3 +105,42 @@ def parse_lottery(s):
     prepared_s = re.sub(r"\d+(\.\d+)?%", lambda m: str(float(m.group()[:-1]) / 100), prepared_s)
     structure = eval(prepared_s)
     return Lottery(structure)
+
+# ===
+# Agents Implementation:
+# ===
+
+class Agent:
+    """
+    A base class for implementing inteligent agents.
+    """
+    
+    def __init__(self, name, lottery):
+        self.name = name
+        self.lottery = lottery
+        
+# Single Agent Decision:
+        
+class RationalAgent(Agent):
+    pass
+
+class SafeAgent(Agent):
+    pass
+
+# Multi Agent Decision:
+
+class ConditionalAgent(Agent):
+    pass
+
+class NashAgent(Agent):
+    pass
+
+class MixedAgen(Agent):
+    pass
+
+# ===
+# Console for interaction:
+# ===
+
+class Console:
+    pass
