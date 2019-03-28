e5a59f20988d7a745e0fca7e48095d0f3a415d2d
Kyntaz <ghost.615b@gmail.com>
2019-03-28 23:30:52 +0000

    add console behaviour

--- agents.py
+++ agents.py
@@ -136,6 +136,7 @@ class RationalAgent(Agent):
     """
     
     def decide(self):
+        # Select the task with the highest pondered value.
         task_values = self.lottery.calculate_pondered_values()
         best_task = list(task_values)[0]
         best_value = task_values[best_task]
@@ -184,4 +185,42 @@ class MixedAgent(Agent):
 # ===
 
 class Console:
-    pass
+    """
+    A class that handles interaction with the agents
+    according to the specification.
+    """
+    
+    def __init__(self):
+        self.agent = None
+        
+    def interaction_loop(self):
+        while True:
+            command = input()
+            
+            if command == "exit":
+                return
+            
+            match = re.match(r"(?P<command>^[A-Za-z](?:\w|-)*) (?P<lottery>\(.*\)) (?P<ncalls>\d+)", command)
+            agent_type = match.group("command")
+            lottery = parse_lottery(match.group("lottery"))
+            n_calls = eval(match.group("ncalls"))
+            
+            if agent_type == "decide-rational":
+                self.agent = RationalAgent(agent_type, lottery)
+            else:
+                raise ValueError("Unkown Agent Type.")
+            
+            for i in range(n_calls):
+                print(self.agent.decide())
+                result = input()
+                
+                if result == "exit":
+                    return
+                
+                self.agent.sense(result)
+                
+
+if __name__ == "__main__":
+    console = Console()
+    console.interaction_loop()
+    
